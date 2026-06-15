b0787c335c5ebb4c56a96a8a96650eb1193a7b9b
MatheusBLima7 <matheusblima7@gmail.com>
2026-06-14 22:14:29 -0300

    Novos operadores e polimento geral

--- Calculadora.java
+++ Calculadora.java
@@ -12,25 +12,48 @@ public class Calculadora {
 
         System.out.printf("Digite o primeiro numero: %n");
         num1 = sc.nextDouble();
+        
         System.out.printf("Digite o operador: %n");
         operador = sc.next();
-        System.out.printf("Digite o segundo numero: %n");
-        num2 = sc.nextDouble();
-
+        
         if (operador.equals("+")) {
+            System.out.printf("Digite o segundo numero: %n");
+            num2 = sc.nextDouble();
+
             System.out.printf("O resultado da conta é: %.2f",num1 + num2);
         }
         else if (operador.equals("-")) {
-            System.out.println("O resultado é:");
-            System.out.println(num1 - num2);
+            System.out.printf("Digite o segundo numero: %n");
+            num2 = sc.nextDouble();
+
+            System.out.printf("O resultado da conta é: %.2f",num1 - num2);
         }
         else if (operador.equals("*")) {
-            System.out.println("O resultado é:");
-            System.out.println(num1 * num2);
+            System.out.printf("Digite o segundo numero: %n");
+            num2 = sc.nextDouble();
+
+            System.out.printf("O resultado da conta é: %.2f",num1 * num2);
         }
         else if (operador.equals("/")) {
-            System.out.println("O resultado é:");
-            System.out.println(num1 / num2);
+            System.out.printf("Digite o segundo numero: %n");
+            num2 = sc.nextDouble();
+
+            System.out.printf("O resultado da conta é: %.2f",num1 / num2);
+        }
+        else if (operador.equals("^")) {
+            System.out.printf("Digite o segundo numero: %n");
+            num2 = sc.nextDouble();
+
+            System.out.printf("O resultado da conta é: %.2f%n", Math.pow(num1, num2));
+        }
+        else if (operador.equals("%")) {
+            System.out.printf("Digite o segundo numero: %n");
+            num2 = sc.nextDouble();
+
+            System.out.printf("O resultado da conta é: %.2f%n", num1 * num2 / 100);
+        }
+        else if (operador.equals("√")) {
+            System.out.printf("O resultado da conta é: %.2f%n", Math.sqrt(num1));
         }
         else {
             System.out.println("Operador não identificado.");
